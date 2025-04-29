e7662443121e5815580e0f5e654c1143a7da216d
s7620605 <274504958@qq.com>
2025-04-29 19:39:42 +0800

    第一次推送仓库

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div style={{ display: 'flex' }}>
+      <nav className="sidebar">
+        <div className="sidebar">
+          <button>核心功能</button>
+          <button>设计风格</button>
+          <button>技术栈</button>
+          <button>第三方服务</button>
+          <button>部署与合规</button>
+          <button>代码生成要求</button>
+        </div>
+      </nav>
+      <div>
+        <div>
+          <a href="https://vite.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Vite + React</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test HMR
+          </p>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App
